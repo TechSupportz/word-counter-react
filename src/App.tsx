10502a6404c3cfd1b940cde73beaacf8c86d1374
Nitish <tnitish654@gmail.com>
2022-02-17 13:23:49 +0800

    prev change messed up desktop github url

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App = () => {
 	return (
 		<>
 			<Topbar />
-			<div className="grid h-[70vh] lg:grid-cols-6 lg:grid-rows-5 lg:gap-y-8 mt-10 lg:mt-16">
+			<div className="grid h-[75vh] lg:grid-cols-6 lg:grid-rows-5 lg:gap-y-8 mt-10 lg:mt-16">
 				<div className="lg:col-span-2 lg:row-span-3 h-full">
 					<Counter />
 				</div>
@@ -18,7 +18,7 @@ const App = () => {
 				<div className="h-[50vh] lg:h-full mx-4 mt-7 lg:mt-0 lg:col-span-4 lg:row-span-full lg:order-first">
 					<TextInput />
 				</div>
-			<a href="https://github.com/TechSupportz/word-counter-tsx" className="w-screen lg:w-fit py-2 text-center lg:text-left text-sm  underline text-blue-300 visited:text-blue-300 dark:text-slate-700/70 dark:visited:text-slate-700/70">Check out the github repository here</a>
+			<a href="https://github.com/TechSupportz/word-counter-tsx" className="w-screen lg:w-fit py-2 lg:px-5 text-center lg:col-span-full lg:text-left lg:align-self-bottom text-sm  underline text-blue-300 visited:text-blue-300 dark:text-slate-700/70 dark:visited:text-slate-700/70">Check out the github repository here</a>
 			</div>
 		</>
 	)
